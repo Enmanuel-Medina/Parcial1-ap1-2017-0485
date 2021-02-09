1eb59bfab17f89712e0352a28aa494b52a567ecd
Jose Enmanuel Jaquez Medina <jose_jaquez1@ucne.edu.do>
2021-02-08 18:50:08 -0700

    Creando la Migracion

---
+++ PArcial1-ap1-2017-0485/Migrations/20210209014938_Migracion_Parcial1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PArcial1_ap1_2017_0485.Migrations
+{
+    public partial class Migracion_Parcial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ciudades",
+                columns: table => new
+                {
+                    CuidadId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudades", x => x.CuidadId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ciudades");
+        }
+    }
+}
