cc319a28006dacb4b8df8fd03bedcf3d64a0e5eb
Jose Enmanuel Jaquez Medina <jose_jaquez1@ucne.edu.do>
2021-02-08 18:46:51 -0700

    Agregando la carpeta DAL

---
+++ PArcial1-ap1-2017-0485/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PArcial1_ap1_2017_0485.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PArcial1_ap1_2017_0485.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Ciudades> Ciudades { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlite(@"Data Source=Data\Cuidades.db");
+        }
+    }
+}
